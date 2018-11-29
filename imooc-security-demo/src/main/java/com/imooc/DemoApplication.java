8ecfdd46a36635a0eca036d7332ac4a53e7f5a37
pzhbao <pzhbao@cn.ibm.com>
2018-11-29 18:25:50 +0800

    init project

---
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 描述:
+ *
+ * @author pzb
+ * @create 2018-11-29 5:05 PM
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security demo";
+    }
+}
